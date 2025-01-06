7839389051da4f3bdf4aa9b4b7fb0ce86350a3d6
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2025-01-06 16:03:07 -0300

    Update main.js
    Going to a 3d site...

--- main.js
+++ main.js
@@ -1,16 +1,63 @@
-let count = 1
+import * as THREE from 'three';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
-function prevSlide(){
-    if( count != 1){
-        console.log('ok')
-        count -= 1
-        window.location.href = `http://127.0.0.1:5500/index.html#slide-${count}`;
-    }
+const w = window.innerWidth;
+const h = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(w, h);
+
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w/h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+// mesh
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial({color: 0xff5b00, flatShading: true});
+const mesh = new THREE.Mesh(geo, mat);
+
+scene.add(mesh);
+
+// wireframe
+
+const wireMat = new THREE.MeshStandardMaterial({
+	color: 0xffffff,
+	wireframe: true
+});
+const wireMesh = new THREE.Mesh(geo, wireMat);
+
+mesh.add(wireMesh);
+
+// light
+
+const hemLight = new THREE.HemisphereLight(0xffabcf, 0xaa5699);
+
+scene.add(hemLight);
+
+function animate(t = 0){
+	requestAnimationFrame(animate);
+
+	mesh.rotation.z = t * 0.0001;
+	mesh.rotation.x = t * 0.0001 * Math.cos(t*0.00001);
+
+	mesh.scale.setScalar(Math.cos(t*0.0001)*0.2+1);
+
+	controls.update();
+
+	renderer.render(scene, camera);
 }
-function nextSlide(){
-    if( count != 3){
-        console.log('ok')
-        count += 1
-        window.location.href = `http://127.0.0.1:5500/index.html#slide-${count}`;
-    }
-}
+
+animate();
